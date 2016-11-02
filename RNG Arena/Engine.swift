bff91c3163e4672702b2234773b3184c79dde3dc
hpboy95 <hpboy99@gmail.com>
2016-11-01 21:50:25 -0700

    Test
    Added logic area. Removed test code

---
+++ RNG Arena/Engine.swift
@@ -0,0 +1,13 @@
+//
+//  Engine.swift
+//  RNG Arena
+//
+//  Created by Hezekiah Valdez on 11/1/16.
+//  Copyright © 2016 Hezekiah Valdez. All rights reserved.
+//
+
+class Engine{
+    
+    
+    
+}
