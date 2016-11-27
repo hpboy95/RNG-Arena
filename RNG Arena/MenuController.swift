11991ff53dc0c8cca8d3c409f4f6033b4377b66c
davidpham117 <david@Davids-Mac.local>
2016-11-26 18:18:34 -0800

    added main menu

---
+++ RNG Arena/MenuController.swift
@@ -0,0 +1,26 @@
+//
+//  MenuController.swift
+//  RNG Arena
+//
+//  Created by David on 11/25/16.
+//  Copyright © 2016 Hezekiah Valdez. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
