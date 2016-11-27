dbd303063e59dc934c2d79c86d01124efa5651d1
hpboy95 <hpboy99@gmail.com>
2016-11-26 21:30:13 -0800

    Day's work Save
    implemented mini save game.

--- RNG Arena/RAEngine.swift
+++ RNG Arena/RAEngine.swift
@@ -6,8 +6,45 @@
 //  Copyright © 2016 Hezekiah Valdez. All rights reserved.
 //
 
+import Foundation
+
 class Engine{
     
+    var score: Int
+    var highScore: Int
     
+    var currentData: NSDictionary!
+    
+    class var sharedInstance: Engine {
+        
+        struct Singleton{
+            static let instance = Engine()
+        }
+        
+        return Singleton.instance
+        
+    }
+    
+    
+    init(){
+        score = 0
+        highScore = 0
+        
+        let userDefaults = UserDefaults.standard
+        
+        highScore = userDefaults.integer(forKey: "highScore")
+        
+    }
+    
+    func saveGameStats(){
+        
+        highScore = max(score, highScore)
+        
+        let userDefaults =  UserDefaults.standard
+        userDefaults.set(highScore, forKey: "highScore")
+        
+        userDefaults.synchronize()
+        
+    }
     
 }
